ac4467e0e1a0cb45bdc84b5a3140100daa846447
Trever Johnston <treverj1029@gmail.com>
2017-11-02 13:26:11 -0600

    basics of routes is done

--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -1,23 +1,77 @@
 <template>
-        <q-layout class="about">
-Routes        </q-layout>
-      </template>
-      
-      <script>
-          import {
-              QLayout
-          } from 'quasar'
-      export default {
-          name: 'About',
-        data () {
-          return {}
+    <q-layout class="routes">
+        <div class="promo text-center desktop-only">
+            <h2 class="text-bold">2018 Route Maps Listed Below*</h2>
+            <h4 class="text-bold">(Pending unforseen road construction)</h4>
+            <h4 class="text-bold">(9.5 mile route will be added for 2018)</h4>
+        </div>
+        <div class="row wrap justify-center">
+            <q-card v-for="card in routes" inline class="route col-xs-11 col-sm-5">
+                <q-card-media>
+                    <img :src="card.pic">
+                </q-card-media>
+                <q-card-title>
+                    {{card.length}}
+                </q-card-title>
+                <q-card-actions>
+                    <a :href="card.url" target="_blank">
+                        <q-btn color="primary">Route Map</q-btn>
+                    </a>
+                </q-card-actions>
+            </q-card>
+        </div>
+    </q-layout>
+</template>
+
+<script>
+    import {
+        QLayout,
+        QCard,
+        QCardMain,
+        QCardMedia,
+        QCardTitle,
+        QCardSeparator,
+        QCardActions,
+        QBtn
+    } from 'quasar'
+    export default {
+        name: 'About',
+        data() {
+            return {
+            }
         },
-        components:{
-            QLayout
+        components: {
+            QLayout,
+            QCard,
+            QCardMain,
+            QCardMedia,
+            QCardTitle,
+            QCardSeparator,
+            QCardActions,
+            QBtn
+        },
+        computed: {
+            routes() {
+                return this.$store.state.routes
+            }
+        },
+        methods: {
         }
-      }
-      </script>
-      
-      <style>
-      </style>
-      
+    }
+</script>
+
+<style scoped>
+    .route {
+        /* margin: 0 1rem 0 1rem; */
+    }
+
+    .routes {
+        background-color: rgb(0, 95, 95);
+    }
+
+    .promo {
+        /* margin-top: -6rem; */
+        padding: 0 2rem 0 2rem;
+        background-color: rgba(2, 182, 182, 0.6);
+    }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,38 @@ vue.use(vuex)
 
 var store = new vuex.Store({
     state: {
-        
+        routes:[
+            {
+                url: 'http://www.mapmyride.com/routes/view/1647707687',
+                length: 9.5,
+                pic: 'statics/9.5.png'
+            },
+            {
+                url: 'http://www.mapmyride.com/routes/view/996049745',
+                length: 22,
+                pic: 'statics/22.png'
+            },
+            {
+                url:'http://www.mapmyride.com/routes/view/1478990992',
+                length: 31,
+                pic: 'statics/31.png'
+            },
+            {
+                url: 'http://www.mapmyride.com/routes/view/1478877649',
+                length: 50,
+                pic: 'statics/50.png'
+            },
+            {
+                url: 'http://www.mapmyride.com/routes/view/981667107',
+                length: 65,
+                pic: 'statics/65.png'
+            },
+            {
+                url: 'http://www.mapmyride.com/routes/view/1462908562',
+                length: 100,
+                pic: 'statics/100.png'
+            }
+        ]
     },
     mutations: {
         handleError(state, err) {
