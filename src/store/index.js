a7b19710ec5872e22498a6c85eb4ab10854edd9d
Trever Johnston <treverj1029@gmail.com>
2018-06-12 19:02:44 -0600

    changed raffle picture endings

--- src/store/index.js
+++ src/store/index.js
@@ -42,72 +42,72 @@ var store = new vuex.Store({
             //     description: '(Starlite Lazer 20 Youth Bike (Made to Order: Girls or Boys, various colors available)'
             // },
             {
-                link: '../statics/raffle/IMG_1671.png',
+                link: './statics/raffle/IMG_1671.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1672.png',
+                link: './statics/raffle/IMG_1672.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1673.png',
+                link: './statics/raffle/IMG_1673.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1674.png',
+                link: './statics/raffle/IMG_1674.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1675.png',
+                link: './statics/raffle/IMG_1675.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1676.png',
+                link: './statics/raffle/IMG_1676.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1677.png',
+                link: './statics/raffle/IMG_1677.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1678.png',
+                link: './statics/raffle/IMG_1678.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1679.png',
+                link: './statics/raffle/IMG_1679.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1680.png',
+                link: './statics/raffle/IMG_1680.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1681.png',
+                link: './statics/raffle/IMG_1681.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1682.png',
+                link: './statics/raffle/IMG_1682.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1683.png',
+                link: './statics/raffle/IMG_1683.png',
                 donator: '',
                 description: ''
             },
             {
-                link: '../statics/raffle/IMG_1684.png',
+                link: './statics/raffle/IMG_1684.png',
                 donator: '',
                 description: ''
             },
